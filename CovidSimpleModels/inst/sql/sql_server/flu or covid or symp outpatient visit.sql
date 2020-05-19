c7e981bfdbbb8f93d16a99b1dc88952050767ec9
jreps <jreps@its.jnj.com>
2020-05-19 16:45:03 -0400

    updated to 2020

--- CovidSimpleModels/inst/sql/sql_server/flu or covid or symp outpatient visit.sql
+++ CovidSimpleModels/inst/sql/sql_server/flu or covid or symp outpatient visit.sql
@@ -327,7 +327,7 @@ from
 JOIN #Codesets codesets on ((vo.visit_concept_id = codesets.concept_id and codesets.codeset_id = 14))
 ) C
 
-
+WHERE C.visit_start_date >= DATEFROMPARTS(2020, 01, 01)
 -- End Visit Occurrence Criteria
 
   ) E
